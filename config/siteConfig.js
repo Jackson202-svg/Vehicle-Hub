a66b6f306cd99af790ecdf17239d502fcfff5efe
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2026-02-28 21:39:17 +0900

    Add site configuration file with initial settings

---
+++ config/siteConfig.js
@@ -0,0 +1,10 @@
+// config/siteConfig.js
+export const siteConfig = {
+    teamName: "Palmy Development Team",
+    version: "1.0.0",
+    contactEmail: "info@palmy-dev.com",
+    social: {
+        instagram: "@palmy_vehicles",
+        github: "palmy-dev/vehicle-hub"
+    }
+};
